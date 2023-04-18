b657881953bd881e4cb2c3b4bd30527b54991957
DESKTOP-LOH3OLP\ParkSoYun <soyun.park@mcloudoc.com>
2023-04-18 17:30:30 +0900

    13. Roman to Integer - CSharp

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Submissions_CSharp;
+
+namespace leetcode
+{
+    public static partial class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Submissions.Roman_to_Integer("III"));
+            Console.WriteLine(Submissions.Roman_to_Integer("LVIII"));
+            Console.WriteLine(Submissions.Roman_to_Integer("MCMXCIV"));
+        }
+    }
+}
---
+++ Submissions_CSharp/Class1.cs
@@ -0,0 +1,35 @@
+﻿namespace Submissions_CSharp;
+
+public static class Submissions
+{
+    public static int Roman_to_Integer(string s)
+    {
+        Dictionary<char, int> romanmap=  new Dictionary<char, int>()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        int sum = 0;
+        int hold = 0;
+
+        for(int i = 0; i < s.Count(); i++)
+        {
+            if (i+1 < s.Count() && romanmap[s[i]] < romanmap[s[i+1]])
+            {
+                hold = romanmap[s[i]];
+                continue;
+            }
+
+            sum += romanmap[s[i]] - hold;
+            hold = 0;                
+        }
+
+        return sum;
+    }
+}
